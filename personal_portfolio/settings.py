4ffdc844dda9e24bc68b2e90d949d8d97d4adce6
Willian Amaral <willian.amaral@outlook.com.br>
2019-06-28 22:17:36 -0300

    Prepare for production

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'personal_portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['personal_portfolio/templates/'],
+        'DIRS': ['willamaral.pythonanywhere.com/personal_portfolio/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
